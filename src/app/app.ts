a2180e7ee1fbc69a0afad06ffe7fbb755acab9ea
SebastianRehfeldt <sebastian.rehfeldt@gmx.net>
2015-12-30 22:49:20 +0100

    play nice music from spotify

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,33 @@
-import {bootstrap, Component} from 'angular2/angular2';
+import {bootstrap, Component, NgFor, ChangeDetectionStrategy, ChangeDetectorRef} from 'angular2/angular2';
+
+
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App Yippi</h1>'
+    templateUrl: "app/app.component.html",
+    directives: [NgFor],
+    changeDetection: ChangeDetectionStrategy.OnPushObserve
 })
-class AppComponent { }
+
+export class AppComponent {
+    public playlists:Array<Object>
+
+    constructor(private ref: ChangeDetectorRef){
+        this.playlists = []
+        this.playNiceMusic()
+    }
+
+    playNiceMusic() {
+        SC.resolve("https://soundcloud.com/sebastian-rehfeldt-1").then((user) =>{
+            SC.get("/users/"+user.id+"/playlists").then((response)=>{
+                this.playlists = response
+                this.ref.markForCheck()
+                this.ref.detectChanges()
+                console.log(response)
+                SC.stream("/tracks/"+response[0].tracks[0].id).then(function(player){
+                    player.play()
+                 })
+            })
+        })
+    }
+}
 bootstrap(AppComponent);
