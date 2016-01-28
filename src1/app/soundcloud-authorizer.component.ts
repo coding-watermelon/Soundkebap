5348b81236fc019b1a63b26dc8cb93b421d16243
Markus Petrykowski <github@markus-petrykowski.de>
2016-01-28 21:18:26 +0100

    asdf

--- src1/app/soundcloud-authorizer.component.ts
+++ src1/app/soundcloud-authorizer.component.ts
@@ -24,10 +24,18 @@ export class SoundcloudAuthorizer {
   connectWithSoundcloud(){
     this.clientId = "a1c4188f7622b71c3e7c6cf7567fc488"
     this.callbackUrl = "http://soundkebap.markus-petrykowski.de"
-    window.location.href = `
-    https://soundcloud.com/connect?client_id=` + this.clientId +
-    "&display=popup&redirect_uri=" + this.callbackUrl +
-    "&response_type=code_and_token&scope=non-expiring&state=SoundCloud_Dialog_48a88"
+    // window.location.href = `
+    // https://soundcloud.com/connect?client_id=` + this.clientId +
+    // "&display=popup&redirect_uri=" + this.callbackUrl +
+    // "&response_type=code_and_token&scope=non-expiring&state=SoundCloud_Dialog_48a88"
+
+    SC.initialize({
+      client_id: 'a1c4188f7622b71c3e7c6cf7567fc488',
+      redirect_uri: 'http://soundkebap.markut-petrykowski.de' //ToDo: callbackFile needs to be registered on yourApps on soundcloud
+    });
+    SC.connect().then(function(){
+        console.log("login")
+    })
   }
 
 
