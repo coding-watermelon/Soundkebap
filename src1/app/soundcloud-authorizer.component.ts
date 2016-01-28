e9988d1fb666cae21597cc338b35dc4daf40a4f5
Markus Petrykowski <github@markus-petrykowski.de>
2016-01-28 20:57:57 +0100

    changed callback url

--- src1/app/soundcloud-authorizer.component.ts
+++ src1/app/soundcloud-authorizer.component.ts
@@ -23,9 +23,10 @@ export class SoundcloudAuthorizer {
 
   connectWithSoundcloud(){
     this.clientId = "a1c4188f7622b71c3e7c6cf7567fc488"
+    this.callbackUrl = "http://soundkebap.markus-petrykowski.de"
     window.location.href = `
     https://soundcloud.com/connect?client_id=` + this.clientId +
-    "&display=popup&redirect_uri=http%3A%2F%2Flocalhost%3A7070" +
+    "&display=popup&redirect_uri=" + this.callbackUrl +
     "&response_type=code_and_token&scope=non-expiring&state=SoundCloud_Dialog_48a88"
   }
 
